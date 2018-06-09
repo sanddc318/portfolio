d03aa40d522805bf23ddfd89aee5f225237b1d16
Dontavious Sanders <sanddc318@gmail.com>
2018-06-08 22:34:56 -0500

    Organized styles. SplashPage pretty much finito. Will add nav later

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -3,10 +3,12 @@ import React from 'react'
 const SplashPage = () => (
   <div className="outer-box">
     <div className="container">
-      <h1 className="inner-box__intro">Sup. My name is Dontavious Sanders.</h1>
+      <h1 className="inner-box__intro">Hey. The name's Dontavious.</h1>
       <h1 className="inner-box__intro inner-box__intro--about">
         I'm a developer based in Dallas, TX.
       </h1>
+
+      <p className="inner-box__secret">Psst. Keep scrolling, there's more.</p>
     </div>
   </div>
 )
