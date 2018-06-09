15da580355ce8edb09be491a871ad4c5b2eb9ca4
Dontavious Sanders <sanddc318@gmail.com>
2018-06-08 22:16:47 -0500

    Cleaned up css a little

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SplashPage = () => (
   <div className="outer-box">
-    <div className="inner-box">
+    <div className="container">
       <h1 className="inner-box__intro">Sup. My name is Dontavious Sanders.</h1>
       <h1 className="inner-box__intro inner-box__intro--about">
         I'm a developer based in Dallas, TX.
