0c8e69fdbf6611e01a1548e67bfbc9be45884311
Dontavious Sanders <sanddc318@gmail.com>
2018-06-08 23:04:36 -0500

    Last commit for the night. Will work on About page tomorrow

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => (
+  <div className="outer-box outer-box--inverted">
+    <div className="container container--inverted">
+      <h1 className="section-header" style={{ textTransform: 'lowercase' }}>
+        &nbsp; $ whoami &nbsp;
+      </h1>
+    </div>
+  </div>
+)
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import SplashPage from './SplashPage'
+import About from './About'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <SplashPage />
+        <About />
       </div>
     )
   }
