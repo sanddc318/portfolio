6438329df6dc73622496029198448f3013978ba2
Dontavious Sanders <sanddc318@gmail.com>
2018-06-11 14:26:24 -0500

    Deleted uneccesary files / folders

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'normalize.css'
-
-import App from './components/App'
-import './styles/styles.scss'
-
-ReactDOM.render(<App />, document.getElementById('root'))
