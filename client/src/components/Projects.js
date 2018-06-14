082c9fdb44318149669e19d902d2a9cc05bed0f3
Dontavious Sanders <sanddc318@gmail.com>
2018-06-14 09:32:25 -0500

    Filled in the projects page with some projects

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,9 +1,44 @@
 import React from 'react'
 
+import projects from '../utils/projectsAPI'
+
 const Projects = () => (
   <div className="outer-box outer-box--inverted" id="projects">
     <div className="container container--inverted">
       <h1 className="component-header">&nbsp; Projects &nbsp;</h1>
+
+      <div className="projects-grid">
+        {projects.map(project => {
+          const backgroundStyles = {
+            backgroundSize: 'cover',
+            border: '5px solid #333',
+            filter: 'grayscale()',
+            height: '160px'
+          }
+
+          return (
+            <section className="project" key={project.name}>
+              <h2>{project.name}</h2>
+
+              <div style={backgroundStyles} className="project-thumbnail">
+                <img
+                  src={require(`../public/images/${project.image}`)}
+                  alt="Project thumbnail"
+                />
+              </div>
+
+              <a href={project.url} className="project-link" target="_blank">
+                Live
+              </a>
+              <a href={project.github} className="project-link" target="_blank">
+                GitHub
+              </a>
+
+              <p className="project-description">{project.description}</p>
+            </section>
+          )
+        })}
+      </div>
     </div>
   </div>
 )
---
+++ client/src/utils/projectsAPI.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    name: 'feedTorrent',
+    image: 'feedTorrent_thumbnail.png',
+    url: 'https://feedtorrent.herokuapp.com/index.php',
+    github: 'https://github.com/sanddc318/swirlfeed',
+    description:
+      'One of my earlier projects. Built with PHP 7 and ClearDB MySQL. Can login / signup, create posts, add friends and send messages. Has other nice social media features too.'
+  },
+  {
+    name: 'Bugger',
+    image: 'bugger_thumbnail.png',
+    url: 'https://bugger-80571.firebaseapp.com/bugs',
+    github: 'https://github.com/sanddc318/angular2-bug-tracker',
+    description:
+      'Essentially a CRUD app for logging bugs. Used Angular 2 and a Firebase backend. Another one of my older projects.'
+  }
+]
